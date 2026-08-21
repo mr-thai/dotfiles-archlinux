86e0cd7962a9640d7eed7caa623bdd9b7d338103
Đặng Ngọc Thái <mr.thai2k5@gmail.com>
2026-08-22 06:47:25 +0700

    Auto-backup: 2026-08-22 06:47:25

--- nvim/.config/nvim/lua/plugins/1_lazyvim/web_dev.lua
+++ nvim/.config/nvim/lua/plugins/1_lazyvim/web_dev.lua
@@ -1,35 +1,31 @@
 return {
-    {
-        "windwp/nvim-ts-autotag",
-        event = { "InsertEnter" }, -- Đảm bảo plugin được chạy khi gõ code
-        opts = {
-            enable_close = true,
-            enable_rename = true,
-            enable_close_on_slash = true,
-        },
-    },
+  {
+    "windwp/nvim-ts-autotag",
+    event = { "InsertEnter" }, -- Đảm bảo plugin được chạy khi gõ code
+    opts = {},
+  },
 
-    {
-        "neovim/nvim-lspconfig",
-        opts = {
-            servers = {
-                emmet_language_server = {
-                    filetypes = {
-                        "css",
-                        "eruby",
-                        "html",
-                        "javascriptreact",
-                        "less",
-                        "sass",
-                        "scss",
-                        "svelte",
-                        "pug",
-                        "typescriptreact",
-                        "vue",
-                        "php",
-                    },
-                },
-            },
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        emmet_language_server = {
+          filetypes = {
+            "css",
+            "eruby",
+            "html",
+            "javascriptreact",
+            "less",
+            "sass",
+            "scss",
+            "svelte",
+            "pug",
+            "typescriptreact",
+            "vue",
+            "php",
+          },
         },
+      },
     },
+  },
 }
